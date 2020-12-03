1c81539a1ac89dfcb639501105f2e1001bdaf953
Robin <hello@robinblomberg.se>
2020-12-03 18:29:21 +0100

    Add more comments

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -5,6 +5,11 @@ export class Url {
     /**
      * @param {...string} urls
      * @return {string}
+     * @example
+     * equal(
+     *   Url.join('/foo//bar/index.php/', 'http://api/User/[userId]/'),
+     *   '/foo//bar/index.php/User/[userId]'
+     * );
      */
     static join(...urls: string[]): string;
     /**
@@ -12,16 +17,50 @@ export class Url {
      * @return {string}
      * @see https://en.wikipedia.org/wiki/URI_normalization#Normalizations_that_preserve_semantics
      * @see https://tools.ietf.org/html/rfc3986
+     * @example
+     * equal(
+     *   Url.normalize('HTTPS://User＠Example.COM:443/%7Efoo%2a/./bar/baz/../qux'),
+     *   'https://User＠example.com/~foo%2A/bar/qux'
+     * );
      */
     static normalize(url: string): string;
     /**
      * @param {string} url
      * @return {UrlSchema}
+     * @example
+     * equal(
+     *   Url.parse('http://localhost:3000/test/index.php?id=36&a=b#top'),
+     *   {
+     *     hash: '#top',
+     *     host: 'localhost:3000',
+     *     hostname: 'localhost',
+     *     href: 'http://localhost:3000/test/index.php?id=36&a=b#top',
+     *     origin: 'http://localhost:3000',
+     *     password: '',
+     *     pathname: '/test/index.php',
+     *     port: '3000',
+     *     protocol: 'http:',
+     *     search: '?id=36&a=b',
+     *     searchParams: {
+     *       a: 'b',
+     *       id: '36'
+     *     },
+     *     username: ''
+     *   }
+     * );
      */
     static parse(url: string): UrlSchema;
     /**
      * @param {string} query
      * @return {Object.<string, string>}
+     * @example
+     * equal(
+     *   Url.parseQuery('?id=36&a=b'),
+     *   {
+     *     a: 'b',
+     *     id: '36'
+     *   }
+     * );
      */
     static parseQuery(query: string): {
         [x: string]: string;
@@ -32,21 +71,49 @@ export class Url {
      * @param {string} url
      * @return {string[]}
      * @example
-     * Url.split('/foo//bar/index.php/');
-     * // ['foo', '', 'bar', 'index.php']
+     * equal(
+     *   Url.split('/foo//bar/index.php/'),
+     *   ['foo', '', 'bar', 'index.php']
+     * );
      *
-     * Url.split('http://localhost:3000/test/index.php?id=36&a=b#top');
-     * // ['http://localhost:3000', 'test', 'index.php']
+     * equal(
+     *   Url.split('http://localhost:3000/test/index.php?id=36&a=b#top'),
+     *   ['http://localhost:3000', 'test', 'index.php']
+     * );
      */
-    static split(url: string): string[];
+    static split(url?: string): string[];
     /**
      * @param {Partial<UrlSchema>} [options]
      * @return {string}
+     * @example
+     * equal(
+     *   Url.stringify({
+     *     hash: 'jumbotron',
+     *     hostname: 'example.com:8083',
+     *     password: 'doe',
+     *     port: 8081,
+     *     protocol: 'https',
+     *     searchParams: {
+     *       all: true,
+     *       updated_since: new Date('2000-01-01').valueOf()
+     *     },
+     *     username: 'john'
+     *   }),
+     *   'https://john:doe＠example.com:8081/?all=true&updated_since=946684800000#jumbotron'
+     * );
      */
     static stringify({ hash, host, hostname, href, origin, password, pathname, port, protocol, search, searchParams, username }?: Partial<import("./types").UrlSchema> | undefined): string;
     /**
      * @param {Query} query
      * @return {string}
+     * @example
+     * equal(
+     *   Url.stringifyQuery({
+     *     'a b': 'my image.png',
+     *     foo: 'test'
+     *   }),
+     *   'a%20b=my%20image.png&foo=test'
+     * );
      */
     static stringifyQuery(query?: Query): string;
 }
--- src/index.js
+++ src/index.js
@@ -18,6 +18,11 @@ export class Url {
   /**
    * @param {...string} urls
    * @return {string}
+   * @example
+   * equal(
+   *   Url.join('/foo//bar/index.php/', 'http://api/User/[userId]/'),
+   *   '/foo//bar/index.php/User/[userId]'
+   * );
    */
   static join(...urls) {
     if (urls.length === 0) {
@@ -67,6 +72,11 @@ export class Url {
    * @return {string}
    * @see https://en.wikipedia.org/wiki/URI_normalization#Normalizations_that_preserve_semantics
    * @see https://tools.ietf.org/html/rfc3986
+   * @example
+   * equal(
+   *   Url.normalize('HTTPS://User＠Example.COM:443/%7Efoo%2a/./bar/baz/../qux'),
+   *   'https://User＠example.com/~foo%2A/bar/qux'
+   * );
    */
   static normalize(url) {
     const parsedUrl = this.parse(url);
@@ -141,6 +151,27 @@ export class Url {
   /**
    * @param {string} url
    * @return {UrlSchema}
+   * @example
+   * equal(
+   *   Url.parse('http://localhost:3000/test/index.php?id=36&a=b#top'),
+   *   {
+   *     hash: '#top',
+   *     host: 'localhost:3000',
+   *     hostname: 'localhost',
+   *     href: 'http://localhost:3000/test/index.php?id=36&a=b#top',
+   *     origin: 'http://localhost:3000',
+   *     password: '',
+   *     pathname: '/test/index.php',
+   *     port: '3000',
+   *     protocol: 'http:',
+   *     search: '?id=36&a=b',
+   *     searchParams: {
+   *       a: 'b',
+   *       id: '36'
+   *     },
+   *     username: ''
+   *   }
+   * );
    */
   static parse(url) {
     const match = url.match(URL_REGEXP) ?? [];
@@ -178,6 +209,14 @@ export class Url {
   /**
    * @param {string} query
    * @return {Object.<string, string>}
+   * @example
+   * equal(
+   *   Url.parseQuery('?id=36&a=b'),
+   *   {
+   *     a: 'b',
+   *     id: '36'
+   *   }
+   * );
    */
   static parseQuery(query) {
     if (query[0] === '?') {
@@ -203,11 +242,15 @@ export class Url {
    * @param {string} url
    * @return {string[]}
    * @example
-   * Url.split('/foo//bar/index.php/');
-   * // ['foo', '', 'bar', 'index.php']
+   * equal(
+   *   Url.split('/foo//bar/index.php/'),
+   *   ['foo', '', 'bar', 'index.php']
+   * );
    *
-   * Url.split('http://localhost:3000/test/index.php?id=36&a=b#top');
-   * // ['http://localhost:3000', 'test', 'index.php']
+   * equal(
+   *   Url.split('http://localhost:3000/test/index.php?id=36&a=b#top'),
+   *   ['http://localhost:3000', 'test', 'index.php']
+   * );
    */
   static split(url = '') {
     const { protocol } = this.parse(url);
@@ -224,6 +267,22 @@ export class Url {
   /**
    * @param {Partial<UrlSchema>} [options]
    * @return {string}
+   * @example
+   * equal(
+   *   Url.stringify({
+   *     hash: 'jumbotron',
+   *     hostname: 'example.com:8083',
+   *     password: 'doe',
+   *     port: 8081,
+   *     protocol: 'https',
+   *     searchParams: {
+   *       all: true,
+   *       updated_since: new Date('2000-01-01').valueOf()
+   *     },
+   *     username: 'john'
+   *   }),
+   *   'https://john:doe＠example.com:8081/?all=true&updated_since=946684800000#jumbotron'
+   * );
    */
   static stringify({
     hash = '',
@@ -293,6 +352,14 @@ export class Url {
   /**
    * @param {Query} query
    * @return {string}
+   * @example
+   * equal(
+   *   Url.stringifyQuery({
+   *     'a b': 'my image.png',
+   *     foo: 'test'
+   *   }),
+   *   'a%20b=my%20image.png&foo=test'
+   * );
    */
   static stringifyQuery(query = {}) {
     const pairs = Object.entries(query);
@@ -301,6 +368,7 @@ export class Url {
       return '';
     }
 
+    // Sort query alphabetically:
     pairs.sort(([a], [b]) => {
       if (a < b) {
         return -1;
